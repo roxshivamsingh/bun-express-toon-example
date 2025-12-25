ec5b07fe29751f01275f817ea0b17bb1b20d29d5
Shivam Singh <roxshivamsingh@gmail.com>
2025-12-25 13:20:24 +0530

    ADDED: Routes > Register > Controller - toon and json;

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import express from "express";
 // ====================================
+import { DeveloperController } from "./controllers";
 const app = express();
 const port = 3000;
+
+app.get("/format/toon", DeveloperController.all);
+app.get("/format/json", DeveloperController.json);
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
