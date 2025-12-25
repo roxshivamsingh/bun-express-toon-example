0b16fa6806de328a32f4750938f3a5437ad336d7
Shivam Singh <roxshivamsingh@gmail.com>
2025-12-25 13:31:24 +0530

    FIXED: Imports;

--- src/controllers/developer.controller.ts
+++ src/controllers/developer.controller.ts
@@ -1,13 +1,13 @@
-import { Request, Response } from "express";
-import toon from "@toon-format/toon";
+import * as express from "express";
+import * as toon from "@toon-format/toon";
 
 import { EXAMPLE_MOCK } from "../mocks";
 
 export class DeveloperController {
-  public static async all(req: Request, res: Response) {
+  public static async all(req: express.Request, res: express.Response) {
     res.send(toon.encode(EXAMPLE_MOCK.developers));
   }
-  public static async json(req: Request, res: Response) {
+  public static async json(req: express.Request, res: express.Response) {
     res.send(EXAMPLE_MOCK.developers);
   }
 }
